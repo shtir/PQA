dfca3a15f71ea59e5a438a20db9abf289ac84d7b
SHahab <shahab.1369@gmail.com>
2021-07-11 06:38:22 +0430

    prepare ID Selector

---
+++ idselector.py
@@ -0,0 +1,92 @@
+class idselector:
+
+    def __init__(self,id):
+        if (id == "1"):
+            self.ipAddress = "192.168.88.10"
+            self.port = 8080
+            self.dbname = "H1V1A01"
+        if (id == "2"):
+            self.ipAddress = "192.168.88.11"
+            self.port = 8080
+            self.dbname = "F1V1A01"
+        if (id == "3"):
+            self.ipAddress = "192.168.88.12"
+            self.port = 8080
+            self.dbname = "A1V1A02"
+        if (id == "4"):
+            self.ipAddress = "192.168.88.13"
+            self.port = 8080
+            self.dbname = "A1V1A01"
+        if (id == "5"):
+            self.ipAddress = "192.168.88.14"
+            self.port = 8080
+            self.dbname = "D1V1A01"
+        if (id == "6"):
+            self.ipAddress = "192.168.88.15"
+            self.port = 8080
+            self.dbname = "MainLab1"
+        if (id == "7"):
+            self.ipAddress = "192.168.88.16"
+            self.port = 8080
+            self.dbname = "MainLab2"
+        if (id == "8"):
+            self.ipAddress = "192.168.88.30"
+            self.port = 8080
+            self.dbname = "Diesel"
+        if (id == "9"):
+            self.ipAddress = "192.168.88.31"
+            self.port = 8080
+            self.dbname = "B1"
+        if (id == "10"):
+            self.ipAddress = "192.168.88.32"
+            self.port = 8080
+            self.dbname = "B2"
+        if (id == "11"):
+            self.ipAddress = "192.168.88.33"
+            self.port = 8080
+            self.dbname = "B3"
+        if (id == "12"):
+            self.ipAddress = "192.168.88.34"
+            self.port = 8080
+            self.dbname = "T1"
+        if (id == "13"):
+            self.ipAddress = "192.168.88.35"
+            self.port = 8080
+            self.dbname = "T2"
+        if (id == "14"):
+            self.ipAddress = "192.168.88.36"
+            self.port = 8080
+            self.dbname = "T3"
+        if (id == "15"):
+            self.ipAddress = "192.168.88.37"
+            self.port = 8080
+            self.dbname = "LMDP"
+        if (id == "16"):
+            self.ipAddress = "192.168.88.38"
+            self.port = 8080
+            self.dbname = "ELMDP"
+        if (id == "17"):
+            self.ipAddress = "192.168.88.39"
+            self.port = 8080
+            self.dbname = "sole_sefid"
+        if (id == "18"):
+            self.ipAddress = "192.168.88.28"
+            self.port = 8080
+            self.dbname = "Ice_bank"
+        if (id == "19"):
+            self.ipAddress = "192.168.88.25"
+            self.port = 8080
+            self.dbname = "Comp1"
+        if (id == "20"):
+            self.ipAddress = "192.168.88.26"
+            self.port = 8080
+            self.dbname = "Comp2"
+        if (id == "21"):
+            self.ipAddress = "192.168.88.27"
+            self.port = 8080
+            self.dbname = "Comp3"
+        if (id == "22"):
+            self.ipAddress = "192.168.88.24"
+            self.port = 8080
+            self.dbname = "MPP"
+
